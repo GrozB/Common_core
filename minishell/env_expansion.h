8a2e8f8758f8a42abf2df2b95182477279af1fe7
GrozB <161513478+GrozB@users.noreply.github.com>
2025-04-18 19:45:40 +0200

    Delete minishell/env_expansion.h

--- minishell/env_expansion.h
+++
@@ -1,7 +0,0 @@
-#ifndef ENV_EXPANSION_H
-# define ENV_EXPANSION_H
-
-char *expand_variables(const char *str, int last_exit_status);
-char	*process_token(const char *token, int last_exit_status);
-
-#endif
